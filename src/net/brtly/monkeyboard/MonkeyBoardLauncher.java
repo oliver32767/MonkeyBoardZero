a17c11a1fb918afcaf91cd559ed11a517923b120
Oliver <oliver@brtly.net>
2011-12-23 23:23:43 -0800

    removing explicit OS X menu bar support from launcher class

--- src/net/brtly/monkeyboard/MonkeyBoardLauncher.java
+++ src/net/brtly/monkeyboard/MonkeyBoardLauncher.java
@@ -10,23 +10,23 @@ public class MonkeyBoardLauncher {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		try {
-		    System.setProperty("apple.laf.useScreenMenuBar", "true");
-		    System.setProperty("com.apple.mrj.application.apple.menu.about.name", "MonkeyBoard");
-	        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		}
-		catch(ClassNotFoundException e) {
-	        System.out.println("ClassNotFoundException: " + e.getMessage());
-		}
-		catch(InstantiationException e) {
-	        System.out.println("InstantiationException: " + e.getMessage());
-		}
-		catch(IllegalAccessException e) {
-	        System.out.println("IllegalAccessException: " + e.getMessage());
-		}
-		catch(UnsupportedLookAndFeelException e) {
-	        System.out.println("UnsupportedLookAndFeelException: " + e.getMessage());
-		}
+//		try {
+//		    System.setProperty("apple.laf.useScreenMenuBar", "true");
+//		    System.setProperty("com.apple.mrj.application.apple.menu.about.name", "MonkeyBoard");
+//	        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//		}
+//		catch(ClassNotFoundException e) {
+//	        System.out.println("ClassNotFoundException: " + e.getMessage());
+//		}
+//		catch(InstantiationException e) {
+//	        System.out.println("InstantiationException: " + e.getMessage());
+//		}
+//		catch(IllegalAccessException e) {
+//	        System.out.println("IllegalAccessException: " + e.getMessage());
+//		}
+//		catch(UnsupportedLookAndFeelException e) {
+//	        System.out.println("UnsupportedLookAndFeelException: " + e.getMessage());
+//		}
 		
 		
 		EventQueue.invokeLater(new Runnable() {
