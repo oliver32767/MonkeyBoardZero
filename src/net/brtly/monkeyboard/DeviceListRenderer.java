5bc3cf660f304cab865381a7168f6bc195ab24c3
Oliver <oliver@brtly.net>
2011-12-25 22:49:44 -0800

    added copyright info to source and quick-fixed restart adb handler

--- src/net/brtly/monkeyboard/DeviceListRenderer.java
+++ src/net/brtly/monkeyboard/DeviceListRenderer.java
@@ -1,3 +1,19 @@
+/*
+ * MonkeyBoard Copyright (C) 2011 Oliver Bartley
+ * 
+ * MonkeyBoard is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * MonkeyBoard is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MonkeyBoard.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.brtly.monkeyboard;
 
 import java.awt.Component;
